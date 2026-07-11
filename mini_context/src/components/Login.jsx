efdfc67fd55256461280465d9d9320944ee2c114
Arpit Rathore <arpitrathore024@gmail.com>
2026-07-11 18:41:54 +0530

    Context Api

---
+++ mini_context/src/components/Login.jsx
@@ -0,0 +1,26 @@
+import React,{useState,useContext} from "react";
+import UserContext from "../context/UserContext";
+
+function Login(){
+    const [username,SetUsername]=useState('')
+    const [password,Setpassword]=useState('')
+    const {setUser}=useContext(UserContext)
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setUser({username,password})
+    }
+    return(
+        <div>
+            <h2>Login</h2>
+            <input type="text" 
+            value={username} onChange={(e)=> SetUsername(e.target.value)}
+            placeholder="username" />
+            <input type="text"
+             value={password} onChange={(e)=> Setpassword(e.target.value)}
+            placeholder="password" />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
